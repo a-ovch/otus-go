5a081afe12cd9b5d44be5a8ab8e14ce6b21c862d
Andrey Ovchinnikov <a.ovch90@gmail.com>
2021-09-23 00:37:04 +0300

    1. Go mod initialized 2. Sample string reversing has been implemented using helper package stringutil

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	const msg = "Hello, OTUS!"
+
+	fmt.Print(stringutil.Reverse(msg))
 }
